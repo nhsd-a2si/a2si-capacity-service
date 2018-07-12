01c1058c39ac1708b9f81437f061842db2a82d7c
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-07-12 08:36:26 +0100

    The App connects to AWS Redis over SSH

--- src/main/java/com/nhsd/a2si/capacityservice/configuration/RedisLocalDockerCapacityRepositoryConfiguration.java
+++ src/main/java/com/nhsd/a2si/capacityservice/configuration/RedisLocalDockerCapacityRepositoryConfiguration.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration.LettuceClientConfigurationBuilder;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -27,32 +32,24 @@ public class RedisLocalDockerCapacityRepositoryConfiguration {
     @Value("${jedis.port}")
     private Integer jedisPort;
 
-    @Value("${jedis.ssl}")
-    private boolean jedisUseSsl;
-
     @Bean
-    JedisPoolConfig jedisPoolConfig() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(1000);
-        jedisPoolConfig.setMaxWaitMillis(1000);
-        jedisPoolConfig.setTestOnReturn(true);
-        return jedisPoolConfig;
+    LettuceClientConfiguration lettuceClientConfiguration(){
+        LettuceClientConfigurationBuilder configurationBuilder = LettuceClientConfiguration.builder();
+        return configurationBuilder.commandTimeout(Duration.ofSeconds(2))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
     }
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig());
-        jedisConnectionFactory.setHostName(jedisHost);
-        jedisConnectionFactory.setPort(jedisPort);
-        jedisConnectionFactory.setUseSsl(jedisUseSsl);
-        return jedisConnectionFactory;
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(jedisHost, jedisPort), lettuceClientConfiguration());
     }
 
     @Bean
     public RedisTemplate<String, CapacityInformation> redisTemplate() {
         RedisTemplate<String, CapacityInformation> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 
