df31a41b44b084f57e220a1b7c94f07375bf9e25
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-10-25 13:32:18 +0100

    Removes storage from this project into its own A2SI-Capacity-History-Service

--- src/main/java/com/nhsd/a2si/capacityservice/storage/WaitTimeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhsd.a2si.capacityservice.storage;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WaitTimeRepository extends CrudRepository<WaitTime, Long> {
-}
--- src/main/java/com/nhsd/a2si/capacityservice/storage/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package is part of a Prototype.
- *
- * This package is likely to become a project of its own and imported via the classpath.
- *
- * Currently used for sending waiting time data into postgres.
- */
-package com.nhsd.a2si.capacityservice.storage;
