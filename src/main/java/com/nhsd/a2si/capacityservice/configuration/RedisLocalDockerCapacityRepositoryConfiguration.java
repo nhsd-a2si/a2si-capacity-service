001815fd46b748e02d7c5dde5677d1ba3111ce0d
Jonathan Pearce <jonathanpearce@Jonathans-MacBook-Pro.local>
2019-01-25 11:18:48 +0000

    CD-834 - connect to dockerised Redis

--- src/main/java/com/nhsd/a2si/capacityservice/configuration/RedisLocalDockerCapacityRepositoryConfiguration.java
+++ src/main/java/com/nhsd/a2si/capacityservice/configuration/RedisLocalDockerCapacityRepositoryConfiguration.java
@@ -1,49 +1,56 @@
 package com.nhsd.a2si.capacityservice.configuration;
 
 import com.nhsd.a2si.capacityinformation.domain.CapacityInformation;
+
+import io.lettuce.core.cluster.ClusterClientOptions;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration.LettuceClientConfigurationBuilder;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 @Profile({"capacity-service-local-redis", "test-capacity-service-local-redis"})
 public class RedisLocalDockerCapacityRepositoryConfiguration {
 
-    @Value("${jedis.host}")
-    private String jedisHost;
+	 @Value("${spring.redis.cluster.nodes}")
+	 private List<String> redisNodeUrls;
 
-    @Value("${jedis.port}")
-    private Integer jedisPort;
+	    @Bean
+	    LettuceClientConfiguration lettuceClientConfiguration(){
+	        LettuceClientConfiguration.LettuceClientConfigurationBuilder configurationBuilder = LettuceClientConfiguration.builder();
+	        return configurationBuilder.commandTimeout(Duration.ofSeconds(2))
+	                .clientOptions(
+	                        ClusterClientOptions
+	                                .builder()
+	                                .validateClusterNodeMembership(false)
+	                                .build()
+	                ).build();
+	    }
 
-    @Bean
-    LettuceClientConfiguration lettuceClientConfiguration(){
-        LettuceClientConfigurationBuilder configurationBuilder = LettuceClientConfiguration.builder();
-        return configurationBuilder.commandTimeout(Duration.ofSeconds(2))
-                .shutdownTimeout(Duration.ZERO)
-                .build();
-    }
+	    @Bean
+	    public RedisConnectionFactory redisConnectionFactory() {
+	    	
+	    	final RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(redisNodeUrls);
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(jedisHost, jedisPort), lettuceClientConfiguration());
-    }
+	    	return new LettuceConnectionFactory(redisClusterConfiguration
+	        		                           ,lettuceClientConfiguration());
+	    }
 
-    @Bean
-    public RedisTemplate<String, CapacityInformation> redisTemplate() {
-        RedisTemplate<String, CapacityInformation> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
-    }
-
+	    @Bean
+	    public RedisTemplate<String, CapacityInformation> redisTemplate() {
+	        RedisTemplate<String, CapacityInformation> redisTemplate = new RedisTemplate<>();
+	        redisTemplate.setKeySerializer(new StringRedisSerializer());
+	        redisTemplate.setConnectionFactory(redisConnectionFactory());
+	        return redisTemplate;
+	    }
 }
 
