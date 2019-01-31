41d9a9db4a32fd81ec257b8a4b48684c977ef29a
Jonathan Pearce <jonathanpearce@Jonathans-MacBook-Pro.local>
2019-01-31 15:03:35 +0000

    CD-841 - reduce calls to authentication DB for internal API calls

--- src/main/java/com/nhsd/a2si/capacityservice/authenticaiton/UserAuthenticationProvider.java
+++ src/main/java/com/nhsd/a2si/capacityservice/authenticaiton/UserAuthenticationProvider.java
@@ -5,13 +5,10 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.TableCollection;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
-import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -22,13 +19,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Component
 @Profile({"capacity-service-aws-redis", "capacity-service-aws-stub", "capacity-service-local-redis"})
 public class UserAuthenticationProvider implements AuthenticationProvider {
-
-    @Autowired
+	
+	@Autowired
     private AmazonDynamoDB amazonDynamoDB;
 
     @Autowired
@@ -36,15 +35,40 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 
     @Value("${amazon.aws.dynamo.table}")
     private  String dynamoAuthenticationTableName;
-
+    
+    @Value("${dos.proxy.user}")
+    private String DOS_PROXY_USER;
+    
+    /**
+     * Stores authorised users that are internal so that we only call the authentication
+     * database once.
+     */
+    private static Map<String, String> authorisedUsers = new HashMap<>();
 
     @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException 
+    {
         final String username    = authentication.getName();
         final String credentials = authentication.getCredentials().toString();
+        
+        // Check authorised users.
+        if(authorisedUsers.containsKey(username)
+        		&& passwordEncoder.matches(credentials, authorisedUsers.get(username)))
+        {
+        	return new UsernamePasswordAuthenticationToken(username, credentials, Collections.emptySet());
+        }    
+        
         Item item = getItemByUsername(username);
         if(Objects.nonNull(item)) {
-            if(passwordEncoder.matches(authentication.getCredentials().toString(), item.getString("SALTED_PASSWORD"))){
+            if(passwordEncoder.matches(authentication.getCredentials().toString(), item.getString("SALTED_PASSWORD")))
+            {
+            	// If user is internal, add to the authorised users map so we only call
+            	// authentication database once for internal calls
+            	if (DOS_PROXY_USER.equals(item.getString("CLIENT_ID")))
+            	{
+            		authorisedUsers.put(item.getString("USERNAME"), item.getString("SALTED_PASSWORD"));
+            	}
+            	
                 return new UsernamePasswordAuthenticationToken(username, credentials, Collections.emptySet());
             }
         }
@@ -53,7 +77,6 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 
     private Item getItemByUsername(String name) {
         DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
-        TableCollection<ListTablesResult> tables = dynamoDB.listTables();
         Table table = dynamoDB.getTable(dynamoAuthenticationTableName);
         GetItemSpec spec = new GetItemSpec().withPrimaryKey("USERNAME", name);
         return table.getItem(spec);
